0d3d6b5bb2e176f3b18ad0ad8790a3260503773d
Bruno <bgarcia@lcc.uma.es>
2019-10-15 12:22:48 +0200

    Begin implementation of iPerf Agent

---
+++ Tap.Plugins.UMA.AdbAgents/Tap.Plugins.UMA.AdbAgents/Instruments/iPerfAgentInstrument.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Keysight.Tap;
+using Tap.Plugins.UMA.Android.Instruments;
+
+namespace Tap.Plugins.UMA.AdbAgents.Instruments
+{
+    [Display("iPerf Agent", Groups: new string[] { "UMA", "Adb Agents" },
+             Description: "Instrument for controlling the Adb Ping Agent")]
+    [ShortName("ADB_iPerf")]
+    public class iPerfAgentInstrument : Instrument
+    {
+        public enum RoleEnum { Client, Server }
+
+        private const string PACKAGE = "com.uma.iperf";
+        private const string SERVICE = PACKAGE + "/.iPerfService";
+        private const string CLIENT_START = PACKAGE + ".CLIENTSTART";
+        private const string CLIENT_STOP = PACKAGE + ".CLIENTSTOP";
+        private const string SERVER_START = PACKAGE + ".SERVERSTART";
+        private const string SERVER_STOP = PACKAGE + ".SERVERSTOP";
+        private const string EXTRA = PACKAGE + ".PARAMETERS";
+        private static readonly List<string> ignoredKeys = new List<string> { "-c", "-s", "-p", "-t", "-i", "-f" };
+
+        [Display("Adb Instrument")]
+        public AdbInstrument Adb { get; set; }
+
+        public void Start(RoleEnum role, string host, int port, int parallel, string extra, string DeviceId = null)
+        {
+            string start = (role == RoleEnum.Client) ? CLIENT_START : SERVER_START;
+            string cmd = iPerfParameters(role, host, port, parallel, extra);
+            Adb.ExecuteAdbCommand(parameters(start, extras: cmd), DeviceId);
+        }
+
+        public void Stop(RoleEnum role, string DeviceId = null)
+        {
+            string stop = (role == RoleEnum.Client) ? CLIENT_STOP : SERVER_STOP;
+            Adb.ExecuteAdbCommand(parameters(stop, extras: null), DeviceId);
+        }
+
+        private string parameters(string intent, string extras = null)
+        {
+            string res = $"shell am startservice -n {SERVICE} -a {intent}";
+            if (extras != null) { res += $" -e {EXTRA} \"{extras}\""; }
+            return res += " --user 0";
+        }
+
+        private string iPerfParameters(RoleEnum role, string host, int port, int parallel, string extra)
+        {
+            Dictionary<string, string> dict = parsedParameters(role, host, port, parallel, extra);
+            List<string> parameters = new List<string>();
+
+            foreach (var pair in dict)
+            {
+                parameters.Add(string.IsNullOrWhiteSpace(pair.Value) ? pair.Key : $"{pair.Key},{pair.Value}");
+            }
+
+            return string.Join(",", parameters);
+        }
+
+        private Dictionary<string, string> parsedParameters(RoleEnum role, string host, int port, int parallel, string extra)
+        {
+            char[] semicolon = new char[] { ';' };
+            char[] space = new char[] { ' ' };
+            Dictionary<string, string> res = new Dictionary<string, string>();
+            List<string> ignored = new List<string>();
+            
+
+            switch (role)
+            {
+                case RoleEnum.Client: res.Add("-c", host); break;
+                case RoleEnum.Server: res.Add("-s", ""); break;
+            }
+
+            res.Add("-p", port.ToString());
+            res.Add("-t", "999999");
+            res.Add("-i", "1");
+            res.Add("-f", "m");
+
+            if (parallel > 1) { res.Add("-P", parallel.ToString()); }
+
+            foreach (string parameter in extra.Split(semicolon, StringSplitOptions.RemoveEmptyEntries))
+            {
+                string[] tokens = parameter.Split(space, StringSplitOptions.RemoveEmptyEntries);
+                string key = tokens[0].Trim();
+                string value = tokens.Count() > 1 ? tokens[1].Trim() : "";
+
+                if (ignoredKeys.Contains(key))
+                {
+                    ignored.Add(key);
+                }
+                else
+                {
+                    res.Add(key, value);
+                }
+            }
+
+            if (ignored.Count != 0)
+            {
+                Log.Warning($"Found restricted extra parameters: {string.Join(", ", ignored)}. Ignored.");
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tap.Plugins.UMA.AdbAgents/Tap.Plugins.UMA.AdbAgents/Results/iPerfResult.cs
@@ -0,0 +1,100 @@
+﻿// Author:      Bruno Garcia Garcia <bgarcia@lcc.uma.es>
+// Copyright:   Copyright 2019-2022 Universidad de Málaga (University of Málaga), Spain
+//
+// This file cannot be modified or redistributed. This header cannot be removed.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tap.Plugins.UMA.AdbAgents.Results
+{
+    public class iPerfResult : ResultBase
+    {
+        private static Regex regex = new Regex(
+            $"{DATETIME}.*<<< Timestamp: {INT} ; Output: (.*) >>>.*",
+            RegexOptions.Compiled);
+
+        private static Regex iperfRegex = new Regex(
+            $@"\[(.*)] *{FLOAT} *- *{FLOAT} *sec *{FLOAT} *MBytes *{FLOAT} *Mbits/sec(.*)?",
+            RegexOptions.Compiled);
+
+        private static Regex udpRegex = new Regex(
+            $@" *{FLOAT} *ms *{INT} */ *{INT} \({FLOAT}%\)",
+            RegexOptions.Compiled);
+             
+
+        public static string[] COLUMNS = new string[] {
+            "Timestamp", "ICMP_Seq", "Success", "Delay"
+        };
+
+        public override string[] GetColumns() { return COLUMNS; }
+
+        public iPerfResult()
+        {
+            LogTime = DateTime.MinValue;
+            Timestamp = 0;
+            Valid = false;
+        }
+
+        public static bool IsiPerfResult(string line)
+        {
+            return regex.IsMatch(line);
+        }
+
+        public static bool IsSumResult(string line)
+        {
+            return (IsiPerfResult(line) && line.Contains("[SUM]"));
+        }
+
+        public override void Parse(string line)
+        {
+            Match match = regex.Match(line);
+
+            if (match.Success)
+            {
+                LogTime = logcatDate(match.Groups[1].Value);
+                Timestamp = ulong.Parse(match.Groups[2].Value);
+
+                Valid = parseiPerf(match.Groups[3].Value);
+            }
+        }
+
+        private bool parseiPerf(string value)
+        {
+            Match match = iperfRegex.Match(value);
+
+            if (match.Success)
+            {
+
+                return parseUdp("");
+            }
+            else { return false; }
+        }
+
+        private bool parseUdp(string value)
+        {
+            Match match = udpRegex.Match(value);
+
+            if (match.Success)
+            {
+
+                return true;
+            }
+            else { return false; }
+        }
+
+        public override IConvertible GetValue(string column)
+        {
+            switch (column)
+            {
+                case "Timestamp": return Timestamp;
+                default: throw new Exception($"Unrecognized column '{column}'");
+            }
+        }
+    }
+}
