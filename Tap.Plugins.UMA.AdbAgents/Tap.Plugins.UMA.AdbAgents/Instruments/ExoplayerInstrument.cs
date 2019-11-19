59872ce7a509ba31a336d4ec11f2df79b8b3ec19
Bruno <bgarcia@lcc.uma.es>
2019-11-19 15:35:38 +0100

    Initial Exoplayer instrument and step

---
+++ Tap.Plugins.UMA.AdbAgents/Tap.Plugins.UMA.AdbAgents/Instruments/ExoplayerInstrument.cs
@@ -0,0 +1,91 @@
+﻿// Author:      Bruno Garcia Garcia <bgarcia@lcc.uma.es>
+// Copyright:   Copyright 2019-2022 Universidad de Málaga (University of Málaga), Spain
+//
+// This file cannot be modified or redistributed. This header cannot be removed.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTap;
+using Tap.Plugins.UMA.Android.Instruments;
+
+namespace Tap.Plugins.UMA.AdbAgents.Instruments
+{
+    public class KeyEvent
+    {
+        public enum ActionEnum { KeyPress, Wait }
+
+        [Display("Action", Order: 1.0)]
+        public ActionEnum Action { get; set; }
+
+        [Display("Key Code/Delay", Order: 1.0, Description: "https://developer.android.com/reference/android/view/KeyEvent.html")]
+        public int Code { get; set; }
+
+        [Display("Repeat", Order: 2.0)]
+        public int Repeat { get; set; }
+
+        [Display("Comment", Order: 3.0)]
+        public string Comment { get; set; }
+
+        public KeyEvent() { }
+
+        public KeyEvent(ActionEnum action, int code, int repeat, string comment)
+        {
+            this.Action = action; this.Code = code; this.Repeat = repeat; this.Comment = comment;
+        }
+    }
+
+    [Display("Exoplayer", Groups: new string[] { "UMA", "Adb Agents" },
+        Description: "Instrument for controlling the Exoplayer")]
+    public class ExoplayerInstrument: Instrument
+    {
+        private const string PACKAGE = "com.google.android.exoplayer2.demo";
+        private const string ACTIVITY = "SampleChooserActivity";
+        public const string AXINOM = "https://raw.githubusercontent.com/Axinom/dash-test-vectors/master/axinom.exolist.json";
+
+        [Display("Adb Instrument")]
+        public AdbInstrument Adb { get; set; }
+
+        public ExoplayerInstrument() : base()
+        {
+            Name = "Exoplayer";
+        }
+
+        public void Start(string DeviceId = null, string exolist = AXINOM, List<KeyEvent> keys = null)
+        {
+            Adb.ExecuteAdbCommand($"shell am start -n {PACKAGE}/.{ACTIVITY} -d {exolist}", DeviceId);
+            TapThread.Sleep(3000);
+            if (keys != null) { PressKeys(keys); }
+        }
+
+        public void Stop(string DeviceId = null)
+        {
+            Adb.ExecuteAdbCommand($"shell am force-stop {PACKAGE}");
+            TapThread.Sleep(500);
+        }
+
+        public void PressKeys(List<KeyEvent> keys)
+        {
+            foreach (KeyEvent key in keys)
+            {
+                for (int i = 1; i <= key.Repeat; i++)
+                {
+                    if (key.Action == KeyEvent.ActionEnum.KeyPress)
+                    {
+                        Log.Debug($"Pressing key {key.Code} (repeat {i}) '{key.Comment}'");
+                        Adb.ExecuteAdbCommand($"shell input keyevent {key.Code}");
+                        TapThread.Sleep(500);
+                    }
+                    else
+                    {
+                        Log.Debug($"Waiting {key.Code}ms (repeat {i}) '{key.Comment}'");
+                        TapThread.Sleep(key.Code);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tap.Plugins.UMA.AdbAgents/Tap.Plugins.UMA.AdbAgents/Steps/AdbExoplayerStep.cs
@@ -0,0 +1,73 @@
+﻿// Author:      Bruno Garcia Garcia <bgarcia@lcc.uma.es>
+// Copyright:   Copyright 2019-2022 Universidad de Málaga (University of Málaga), Spain
+//
+// This file cannot be modified or redistributed. This header cannot be removed.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+using OpenTap;
+using Tap.Plugins.UMA.AdbAgents.Instruments;
+using Tap.Plugins.UMA.AdbAgents.Results;
+
+namespace Tap.Plugins.UMA.AdbAgents.Steps
+{
+    [Display("Adb Exoplayer", Groups: new string[] { "UMA", "Agents" })]
+    public class AdbExoplayerStep : AdbAgentBaseStep
+    {
+        private static readonly string DEVICE_FILE = "sdcard/adb_exoplayer.log";
+        private static readonly string AGENT_TAG = "TriangleInstr";
+
+        [Display("Agent", Order: 1.0)]
+        public ExoplayerInstrument Instrument { get; set; }
+
+        [EnabledIf("Action", ActionEnum.Start, ActionEnum.Measure, HideIfDisabled = true)]
+        [Display("Exolist", Group: "Configuration", Order: 51.0)]
+        public string Exolist { get; set; }
+
+        [EnabledIf("Action", ActionEnum.Start, ActionEnum.Measure, HideIfDisabled = true)]
+        [Display("Key Events", Group: "Configuration", Order: 51.1,
+            Description: "List of keys to automatically press after requesting the use of the exolist.\n" + 
+                         "Key codes: https://developer.android.com/reference/android/view/KeyEvent.html"
+            )]
+        public List<KeyEvent> KeyEvents { get; set; }
+
+        public AdbExoplayerStep() : base()
+        {
+            Exolist = ExoplayerInstrument.AXINOM;
+            KeyEvents = new List<KeyEvent>() {
+                new KeyEvent(KeyEvent.ActionEnum.KeyPress, 62, 1, "Select exolist"),
+                new KeyEvent(KeyEvent.ActionEnum.KeyPress, 66, 1, "Open exolist"),
+                new KeyEvent(KeyEvent.ActionEnum.KeyPress, 20, 2, "Select Video"),
+                new KeyEvent(KeyEvent.ActionEnum.KeyPress, 66, 1, "Start video"),
+            };
+        }
+
+        public override void Run()
+        {
+            DoRun(Instrument.Adb, DEVICE_FILE, AGENT_TAG);
+        }
+
+        protected override void StartAgent()
+        {
+            Instrument.Start(DeviceId, Exolist, KeyEvents);
+        }
+
+        protected override void StopAgent()
+        {
+            Instrument.Stop(DeviceId);
+        }
+
+        protected override void ParseResults(string[] logcatOutput, DateTime startTime)
+        {
+            //List<PingResult> results = parseResults<PingResult>("ADB Ping Agent", logcatOutput, startTime);
+
+            // Additional processing for aggregated measurements
+            List<string> columns = new List<string>() { "Timestamp", "Total", "Success", "Failed", "Success Ratio", "Failed Ratio" };
+            
+            Results.Publish("ADB Ping Agent Aggregated", columns, 1,1,1,1,1,1,1);
+        }
+    }
+}
